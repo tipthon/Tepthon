1b03b39df0ba4b6f67c5a1da992e05dde71b7dd2
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-05-02 10:13:46 -0700

    Update __main__.py

--- HuRe/__main__.py
+++ HuRe/__main__.py
@@ -24,7 +24,7 @@ print("Licensed under the terms of the " + HuRe.__license__)
 cmdhr = Config.COMMAND_HAND_LER
 
 try:
-    LOGS.info("جارِ بدء بوت الجوكر ✓")
+    LOGS.info("جارِ بدء بوت تيبثون العرب ✓")
     l313l.loop.run_until_complete(setup_bot())
     LOGS.info("تم اكتمال تنصيب البوت ✓")
 except Exception as e:
